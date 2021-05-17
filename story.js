6a8a9fa0e354be20fb3ad6fe623f4dfa4583ce11
Hayder-Abdulsahib <hyder.sahib96@gmail.com>
2021-05-17 15:52:16 +0300

    change

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = "hayder";
+let age = 24;
+let hobby = "Walking"
+console.log(`Mr. ${name}, in 33 years, oyu will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
