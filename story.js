81ddf114a904ae5e83ee91c72793e526f78f0b4d
Hayder-Abdulsahib <hyder.sahib96@gmail.com>
2021-05-17 17:17:01 +0300

    solved

--- story.js
+++ story.js
@@ -1,4 +1,4 @@
 let name = "Laila";
-let age = 50;
+let age = 1;
 let hobby = "Hoarding Books"
 console.log(`Mr. ${name}, in 33 years, oyu will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
